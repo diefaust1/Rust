091d5cdb35d2a5c749c400192e4ce22957230f19
Eric <ericfaust@web.de>
2025-09-30 20:52:26 +0200

    i dont know, debug files

---
+++ coin_toss/target/debug/deps/coin_toss-d7cc5c499a0be101.d
@@ -0,0 +1,5 @@
+C:\Users\ericf\Desktop\Coding\Rust\coin_toss\target\debug\deps\coin_toss-d7cc5c499a0be101.d: src\main.rs
+
+C:\Users\ericf\Desktop\Coding\Rust\coin_toss\target\debug\deps\libcoin_toss-d7cc5c499a0be101.rmeta: src\main.rs
+
+src\main.rs:
---
+++ sort_time/target/debug/deps/sort_time-9f887e344fc042f8.d
@@ -0,0 +1,5 @@
+C:\Users\ericf\Desktop\Coding\Rust\sort_time\target\debug\deps\sort_time-9f887e344fc042f8.d: src\main.rs
+
+C:\Users\ericf\Desktop\Coding\Rust\sort_time\target\debug\deps\libsort_time-9f887e344fc042f8.rmeta: src\main.rs
+
+src\main.rs:
