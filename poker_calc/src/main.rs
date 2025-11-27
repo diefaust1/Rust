ace3edd766013a0aa6d4c08f0bc6fd918fa088f4
Eric <ericfaust@web.de>
2025-11-27 13:47:22 +0100

    added variables

--- poker_calc/src/main.rs
+++ poker_calc/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
 
-    let outs : i32 = 0;
-    println!("Hello, world!");
+    const DECK_SIZE: i32 = 52;
+    let mut outs: i32 = 0;
+    let mut pot: i32 = 0;
+    let mut bet: i32 = 0;
+    let mut card_odd: i32 = 0;
+    let mut pot_odds: i32 = 0;
+
+    println!("{bet}");
 }
