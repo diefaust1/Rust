4446b86f0fbc47b12d93ba539fa36640f340128f
Eric <ericfaust@web.de>
2025-11-28 12:49:38 +0100

    updated poker project

--- cmd_tool/src/main.rs
+++ cmd_tool/src/main.rs
@@ -1,5 +1,13 @@
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let one:&String  = &args[1];
+    let two:&String = &args[2];
+    let three:&String = &args[3];
+    let four: &String = &args[4];
+    
+    println!("1: {one}");
+    println!("2: {two}");
+    println!("3: {three}");
+    println!("4: {four}");
 }
--- poker_calc/src/main.rs
+++ poker_calc/src/main.rs
@@ -1,11 +1,66 @@
+use std::env;
 fn main() {
 
+    //Constants
     const DECK_SIZE: i32 = 52;
-    let mut outs: i32 = 0;
-    let mut pot: i32 = 0;
-    let mut bet: i32 = 0;
-    let mut card_odd: i32 = 0;
-    let mut pot_odds: i32 = 0;
+    const FLOP_SIZE: i32 = 5;
+    const TURN_SIZE: i32 = 6;
+    const RIVER_SIZE: i32 = 7;
 
-    println!("{bet}");
+    //Variables
+    let args: Vec<String> = env::args().collect();
+    let outs:&String  = &args[1];
+    let pot:&String = &args[2];
+    let bet:&String = &args[3];
+    let cout_other_players:&String = &args[4];
+    let stage: &String = &args[5];
+    let mut card_odds: f32 = 0.0;
+    let mut pot_odds: f32 = 0.0;
+
+    
+    
+    if args.len() < 2 
+    {
+        println!("Please enter an argument.");
+        return;
+    }
+
+    
+
+    card_odds = calc_card_odds(FLOP_SIZE, outs, DECK_SIZE);
+    println!("Card odds: {card_odds}");
+    pot_odds = calc_pot_odds(bet, pot);
+    println!("Pot odds: {pot_odds}");
+
+    if card_odds < pot_odds 
+    {
+        println!("Good idea to call");            
+    }
+    else 
+    {
+        println!("Bad idea to call");
+    }
+}
+fn calc_card_odds(stage: i32, outs: i32, deck_size: i32) -> f32
+{
+    let stage: f32 = stage as f32;
+    let outs: f32 = outs as f32;
+    let deck_size: f32 = deck_size as f32;
+    let calc_odds: f32 = (deck_size - outs - stage) / outs;
+    
+    return calc_odds;
+}
+fn calc_pot_odds(bet: i32, pot_size: i32) -> f32
+{
+    let bet: f32 = bet as f32;
+    let pot: f32 = pot_size as f32;
+    let new_pot:f32 = bet + pot;
+
+    let calc_odds: f32 = new_pot / bet;
+    
+    return calc_odds;   
+}
+fn parse_arg_to_i32 ()
+{
+
 }
