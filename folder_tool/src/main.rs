59648f0b4f82af44b48bb2cf8fff9bc59c3ff163
Eric <ericfaust@web.de>
2026-01-21 14:40:32 +0100

    changed project

--- folder_tool/src/main.rs
+++ folder_tool/src/main.rs
@@ -3,11 +3,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let one:&String  = &args[1];
     let two:&String = &args[2];
-    let three:&String = &args[3];
-    let four: &String = &args[4];
+    
     
     println!("1: {one}");
     println!("2: {two}");
-    println!("3: {three}");
-    println!("4: {four}");
 }
